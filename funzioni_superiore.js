d5025d7fa92133d081a93d35b8788f1ce31c6eab
TheFrog36 <artisangiorgio98@gmail.com>
2022-04-27 13:26:35 +0200

    Update funzioni_superiore.js

--- funzioni_superiore.js
+++ funzioni_superiore.js
@@ -74,3 +74,52 @@ function multiply_by_3(element){
 
 console.log(testArray.map(multiply_by_3))
 console.log(testArray.map((element) => element / 2))
+
+
+const testArray4 = [2, 4, 7, -9, 1000, -45, 2, 12, 333, -1000, 123, 34];
+
+const testArray5 = ["rosso", "verde", "giallo", "arancione", "magenta", "blu"];
+
+const testArray6 = [true, "pippo", 204, "verde", 3000, 100, false];
+
+
+/// filtrare testArray4 in modo che rimangono solo i numeri positivi
+function isPositive(numero){
+    return numero >= 0;
+}
+
+console.log('numeri positivi', testArray4.filter(isPositive));
+console.log('numeri positivi', testArray4.filter((numero) => numero >= 0));
+
+/// filtrare testArray4 in modo che rimangono solo i numeri divisibili per 3
+function isDivisible3(numero){
+    return numero % 3 === 0;
+}
+
+console.log('numeri divisibili per 3', testArray4.filter(isDivisible3));
+console.log('numeri dvibili per 3', testArray4.filter((numero) => numero % 3 ===0));
+
+/// filtrare testArray4 in modo che rimangono solo i numeri divisibili per 3 e 4
+
+function isDivisible3or4(number){
+    return (number % 3 === 0 || number % 4 === 0);
+} 
+
+console.log(testArray4.filter(isDivisible3or4));
+console.log(testArray4.filter((number)=> number % 3 === 0 || number % 4 === 0));
+    
+/// filtrare testArray5 in modo che rimangono solo le stringhe più corte di 6 lettere
+
+function lessThan6(word){
+     return word.length < 6;
+}
+console.log('parole più corte di sei', testArray5.filter(lessThan6));
+console.log('parole più corte di sei', testArray5.filter((word)=> word.length < 6)
+/// filtrare testArray5 in modo che rimangono solo le stringhe che contengono la lettera 'n'
+
+/// filtrare testArray6 in modo che rimangano solo i numeri 
+/// mappare testArrays4 dividendo tutti i numeri per 3
+/// mappare testArray4 facendo la radice quadrata di tutti i numeri
+/// mappare testArray4 sommando uno solo ai numeri dispari
+/// mappare testArray5 rendendo maiuscole la prima e l'ultima lettera di ogni parola
+/// mappare testArray5 aggiungendo 
